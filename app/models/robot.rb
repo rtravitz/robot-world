791b704c195746be140301f9a8ffb5b11a6bc103
Ryan Travitz <rtravitz@gmail.com>
2016-10-04 17:15:05 -0600

    creates and reads all

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,9 +1,12 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :name, :city, :state, :department, :id
+
   def initialize(robot_params)
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @id             = robot_params["id"] if robot_params["id"]
     @name           = robot_params["name"]
     @city           = robot_params["city"]
     @state          = robot_params["state"]
@@ -12,16 +15,22 @@ class Robot
 
   def save
     @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);",
-                      @name, @city, @state, @description)
+                      @name, @city, @state, @department)
   end
 
   def self.all
-    robots = @database.execute("SELECT * FROM robots")
+    robots = database.execute("SELECT * FROM robots")
     robots.map do |robot|
       Robot.new(robot)
     end
   end
 
-  
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+
 
 end
